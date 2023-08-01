426c79ae88687e8d627f2a069290c65370604df1
bencampbell90 <bencampbell90@outlook.com>
2023-08-02 09:53:40 +1000

    Final project

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -2,9 +2,26 @@ import { useState } from "react";
 import List from "./List";
 import Form from "./Form";
 import { nanoid } from "nanoid";
+import { ToastContainer, toast } from "react-toastify";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -14,13 +31,34 @@ const App = () => {
     };
     const newItems = [...items, newItem];
     setItems(newItems);
-    console.log(items);
+    setLocalStorage(newItems);
+    toast.success("item added to the list");
+  };
+
+  const removeItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item deleted");
+  };
+
+  const editItem = (id) => {
+    const newItems = items.map((item) => {
+      if (item.id === id) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
-      <List items={items} />
+      <List items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
--- starter/src/ListItem.jsx
+++ starter/src/ListItem.jsx
@@ -1,17 +1,24 @@
-import React from "react";
-
 const ListItem = ({ item, removeItem, editItem }) => {
-  const { name, completed } = item;
+  const { name, completed, id } = item;
   return (
-    <ul>
-      <li className="single-item">
-        <input type="checkbox" onChange={editItem} />
-        <p>{name}</p>
-        <button className="remove-btn" onClick={removeItem}>
-          remove
-        </button>
-      </li>
-    </ul>
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => editItem(id)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: completed && "line-through",
+        }}
+      >
+        {name}
+      </p>
+      <button className="btn remove-btn" onClick={() => removeItem(id)}>
+        remove
+      </button>
+    </div>
   );
 };
 
